be0469d1368f3ad48bbdbf841f2f6e4c3262d16e
idimchevski <igor.dimchevski@endava.com>
2016-01-30 17:20:19 +0100

    tree face the camera

---
+++ Ritualot/Assets/Scripts/TreeFaceDirection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TreeFaceDirection : MonoBehaviour
+    {
+        private Camera _camera;
+        void Awake()
+        {
+            _camera = FindObjectOfType<Camera>();
+        }
+
+        void Update()
+        {
+            this.transform.LookAt(_camera.transform);
+        }
+    }
+}
