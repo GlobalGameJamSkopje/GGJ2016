d0198db9e83de36326dc12b6bc16518e079cafd7
idimchevski <igor.dimchevski@endava.com>
2016-01-30 11:01:18 +0100

    inverse runner

--- Ritualot/Assets/Scripts/Player.cs
+++ Ritualot/Assets/Scripts/Player.cs
@@ -1,12 +1,19 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Player : MonoBehaviour
     {
+        private int _navigationIndex;
+
+        void Awake()
+        {
+            _navigationIndex = EditorApplication.currentScene.StartsWith("Inverse") ? 1 : -1;
+        }
         void Update()
         {
-            transform.Translate(Input.GetAxis("Horizontal") * 10f * Time.deltaTime, 0, 0);
+            transform.Translate(Input.GetAxis("Horizontal") * _navigationIndex * 10f * Time.deltaTime, 0, 0);
 
             transform.position = new Vector3(
                 Mathf.Clamp(transform.position.x, -6, 6),
