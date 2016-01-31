afccb7d61ace2b6e283e608119d2b2604bca27b0
Aggasti <oliver.lazarevski@gmail.com>
2016-01-31 12:07:55 +0100

    Stuff Done

--- Ritualot/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Ritualot/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -5,7 +5,9 @@ namespace UnityStandardAssets._2D
 {
     public class Camera2DFollow : MonoBehaviour
     {
-        public Transform target;
+        public Transform Player1, Player2;
+        private Transform _target;
+        private bool _flag;
         public float damping = 1;
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
@@ -16,11 +18,18 @@ namespace UnityStandardAssets._2D
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
 
+        public GameObject EndScreen, CharacterSelect;
+        public Animator CharacterSelectedAnimator;
+
+
         // Use this for initialization
         private void Start()
         {
-            m_LastTargetPosition = target.position;
-            m_OffsetZ = (transform.position - target.position).z;
+            Player1.GetComponent<Platformer2DUserControl>().enabled = true;
+            Player2.GetComponent<Platformer2DUserControl>().enabled = false;
+            _target = Player1;
+            m_LastTargetPosition = _target.position;
+            m_OffsetZ = (transform.position - _target.position).z;
             transform.parent = null;
         }
 
@@ -29,7 +38,7 @@ namespace UnityStandardAssets._2D
         private void Update()
         {
             // only update lookahead pos if accelerating or changed direction
-            float xMoveDelta = (target.position - m_LastTargetPosition).x;
+            float xMoveDelta = (_target.position - m_LastTargetPosition).x;
 
             bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
 
@@ -42,12 +51,55 @@ namespace UnityStandardAssets._2D
                 m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
 
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+            Vector3 aheadTargetPos = _target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
             transform.position = newPos;
 
-            m_LastTargetPosition = target.position;
+            m_LastTargetPosition = _target.position;
         }
+        
+        public void ChangePlayer()
+        {
+            if (_flag)
+            {
+                _target = Player1;
+                Player1.GetComponent<Platformer2DUserControl>().enabled = true;
+                Player2.GetComponent<Platformer2DUserControl>().enabled = false;
+            }
+               
+           
+            else
+            {
+                _target = Player2;
+                Player1.GetComponent<Platformer2DUserControl>().enabled = false;
+                Player2.GetComponent<Platformer2DUserControl>().enabled = true;
+            }
+                
+
+            _flag = !_flag;
+        }
+        public void Cheat()
+        {
+            EndScreen.SetActive(true);
+
+            Player1.GetComponent<Platformer2DUserControl>().enabled = false;
+            Player2.GetComponent<Platformer2DUserControl>().enabled = false;
+            //Code for Finnised anim ( place it on canvas) 
+            Debug.Log("Code for Finnised anim ( place it on canvas) ");
+
+
+        }
+        public void ActivateCharacterSelect()
+        {
+            EndScreen.SetActive(false);
+            CharacterSelect.SetActive(true);
+        }
+        
+        public void ActivateCharacterSelected()
+        {
+            CharacterSelectedAnimator.SetTrigger("Do");
+        }
+
     }
 }
